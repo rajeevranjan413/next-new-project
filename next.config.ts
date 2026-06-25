3c6498c1dbfcf190a8b340741e820ac017244414
rajeevranjan413 <rajuranjan413@gmail.com>
2026-06-26 01:34:58 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['138d-2409-40d4-11d-347b-69e6-dade-b26d-28fc.ngrok-free.app'],
+  allowedDevOrigins: ['*'],
   /* config options here */
 };
 
